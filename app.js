2770b404bc2d1467693c6aaae2a6ba258c108ae4
taicheng <iamdennisme@gmail.com>
2022-04-05 09:08:11 +0800

    Changes

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const tools = require("./tools");
-const history = require('connect-history-api-fallback');
+const main = require('connect-history-api-fallback');
 const path = require("path");
 const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
-app.use(history());
+app.use(main());
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
 app.post('/upload',tools.upload().single('avatar'),(req,res)=>{
-    res.send({  // 给前端返回的数据
-        file: req.file,
-    });
+    res.json({ data: '上传成功' })
 })
 
 module.exports = app;
